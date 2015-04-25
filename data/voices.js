974259860a3aa1e368d6186b0102984381f1947e
Anselm Hook <anselm@hook.org>
2015-04-24 20:30:25 -0700

    fleshed out some thoughts regarding a voices / media / books / articles / links page... it feels like there should be a page for people and their books that they've created or other media they made it also feels like there should be a page linking to videos and other material that argues the cause - separately and it feels like there should be a page linking to recent articles and news - separate yet again - note unfortunately although it would make the home page more lively we can't put recent activity there   because we want the site to be simulation first; the home page must showcase recent sims

--- data/voices.js
+++ data/voices.js
@@ -2,6 +2,22 @@
 var topic_voices = {
   label:"Voices & Media",
   art:"camping.jpg",
+  children:[
+{ label:"Dianne Ackerman", },
+{ label:"Stewart Brand", },
+{ label:"Charles Darwin", },
+{ label:"Jared Diamond", },
+{ label:"Paul Hawken", },
+{ label:"Derrek Jensen", },
+{ label:"Elizabeth Kolbert", },
+{ label:"Gaia Vince", },
+{ label:"Cadillac Desert", },
+{ label:"Antifragile", },
+{ label:"Intertwingled", },
+{ label:"Stephen Lansing", },
+{ label:"EO Wilson", },
+],
+
   notes:
 
 `eo wilson
@@ -40,4 +56,18 @@ Earth System Modeling Framework (ESMF)
 
 };
 
+/*
+
+articles - perhaps a list of these also? possibly in a different layout view?
+or can we have a streaming news view that is a combo of other feeds? { articles, sims, etc? blog posts? }
+
+http://www.reddit.com/r/science/comments/32ww7t/science_ama_series_im_prof_thomas_malone_from_the/
+http://climatecolab.org/
+http://aeon.co/magazine/science/why-extinction-is-not-the-problem/
+http://grist.org/food/everything-i-thought-i-knew-about-water-in-california-is-wrong/
+http://www.thedailybeast.com/articles/2015/04/19/big-idea-california-is-so-over.html
+http://mic.com/articles/85541/nasa-study-concludes-when-civilization-will-end-and-it-s-not-looking-good-for-us
+http://www.vanityfair.com/culture/2015/04/beverly-hills-drought-parody
+
+*/
 
