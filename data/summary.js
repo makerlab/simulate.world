110398ecf323068576ec2bbe55325dd93ad318d6
Anselm Hook <anselm@hook.org>
2015-08-08 10:27:28 -0700

    added summary not visible to main page yet

---
+++ data/summary.js
@@ -0,0 +1,32 @@
+
+var topic_summary1 = {
+  label:"Summary",
+  art:"hike.jpg",
+  notes:
+"\
+The goal of World Makers is to encourage people to build computer simulations of the world. This includes simulating water, weather, crops, land use policy or anything else. Models can be regional or global, simple sketches or full blown simulations.\n\
+\n\
+The classic game ‘Sim City’ by Will Wright is perhaps the best known example of a computer simulation. It lets people build their own imaginary city from the ground up, placing roads, homes and services and measuring their success against the happiness of the population. The goal here is similar - but real - with real data, real stakeholders and real outcomes.\n\
+\n\
+Simulations are a powerful holistic way of understanding and communicating the complexity of the challenges we are facing. The fact is that we are all stakeholders in a complex natural planetary ecosystem. It defines us, our lives, our values. Without nature humanity as we know it will quickly cease to exist. At the same time we are making poor decisions that are undermining our diverse heritage. We risk runaway effects that may be beyond our ability to control. We are playing a high stakes game of planetary Jenga.\n\
+\n\
+This is a crisis of organization. We routinely spend trillions of dollars of value to make millions of dollars in profit. Consider even just dredging the Louisiana swamplands or cutting the mangrove trees and the impact on New Orleans. Nature simply doesn’t care about our rhetoric or our aspirations. We cannot predict or model outcomes in such a way as to reach consensus. Almost universally the side-effects of our actions dominate over intended consequences. Private interests can pursue short term gains and externalize true costs onto the rest of us. In sum the world has become too complex to understand with current tools.\n\
+\n\
+We simply do not know the outcomes of our decisions. We don’t even have trustworthy predictions of those outcomes. Will the world as we know it even exist after 2100? Or will we see massive ecosystem collapse, human migration, war and other disruptive effects? Or will everything be fine? There is no consensus.\n\
+\n\
+The one phenomena moving as fast as the environmental crisis is the Internet and computing power. The hybrid synthesis of human intuition and brute force computation lets us explore more complex situations in more depth. The simulations we are starting to build will eventually allow the most vulnerable and concerned stakeholders predict outcomes at the intersection of policy, law and land-use in a rigorous manner. By doing so we’re shifting the civic debate from “rhetoric” to “model based” reasoning.\n\
+\n\
+In the future there will be nobody who is not an environmentalist. There will be nobody who isn’t a direct participant in helping save this planet. We will find ourselves needing to get the participation and buy in from millions of voices that are currently disempowered. We will need to find a way to foster diversity. We need to find a way to stop private interests from eating the public good.\n\
+\n\
+Ultimately we’re going to need new models of governance and decision making. It’s clear we’re going to have to hack the systems around us. The structures that got us into this mess are not the same as the structures that are going to get us out. This is how we’re going to save the world.\n\
+"
+};
+
+var topic_summary = {
+  label:"Summary",
+  art:"tanker.jpg",
+  kind:"deck",
+  children:[
+    topic_summary1,
+  ],
+};
