c7eff0ad0939e616e8cde6675ed7b44cc3a8e714
Anselm Hook <anselm@hook.org>
2015-08-08 10:34:46 -0700

    revised event

--- data/meetups.js
+++ data/meetups.js
@@ -4,7 +4,7 @@ var topic_meetups = {
   kind:"brief",
   children:[
     { label:"July 24th 2015 Ephemerisle Sacramento Delta", art:"louisiana.jpg", url:"https://www.facebook.com/events/854250157982630/" },
-    { label:"August 15th 2015 Impact Hub Oakland", art:"camping.jpg", url:"http://meetup.com" },
+    { label:"August 15th 2015 New York", art:"camping.jpg", url:"http://meetup.com" },
     { label:"August 30th 2015 Impact Hub Oakland", art:"sunset.jpg", url:"http://meetup.com" },
   ],
 };
