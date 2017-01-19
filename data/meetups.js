604f7c48c73c1c8077cbdf99199be054e32ee6c9
Anselm Hook <anselm@hook.org>
2017-01-19 14:12:03 -0800

    small change to projects

--- data/meetups.js
+++ data/meetups.js
@@ -5,7 +5,7 @@ var topic_meetups = {
   children:[
     { label:"July 24th 2015 Ephemerisle Sacramento Delta", art:"louisiana.jpg", url:"https://www.facebook.com/events/854250157982630/" },
     { label:"August 15th 2015 New York", art:"camping.jpg", url:"http://meetup.com" },
-    { label:"August 30th 2015 Impact Hub Oakland", art:"sunset.jpg", url:"http://meetup.com" },
+    { label:"October 24th 2015 Oceanus", art:"sunset.jpg", url:"http://www.oceanus-aquacon.com" },
   ],
 };
 
